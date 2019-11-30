8d1808c0a22af2ebc36079f7051b37c469cdf2af
Katlintthk <pajukatlin@gmail.com>
2019-11-30 19:49:32 +0200

    tund

---
+++ Gettersandsetters/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Gettersandsetters
+{
+    class Program
+    {
+
+
+        class Person
+        {
+            string name;
+            string gender;
+            int age;
+            string idcode;
+
+            public Person(string _name, string _gender, int _age, string _idcode)
+            {
+                Name = _name;
+                gender = _gender;
+                Age = _age;
+                Idcode = _idcode;
+
+            }
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public string Gender
+            {
+                get { return gender; }
+                set
+                {
+                    if (value == "male"|| value =="female")
+                    {
+                        gender = value;
+                    }
+                    else
+                    {
+                        gender = "unicorn";
+                    }
+                }
+            }
+
+            public string Idcode
+            {
+                get { return idcode; }
+                set
+                {
+                    if (value.Length== 11)
+                    {
+                        idcode = value;
+                    }
+                    else
+                    {
+                        idcode = " Is not correct";
+                    }
+                }
+            }
+
+            public int Age
+            {
+                get { return age; }
+                set
+                {
+                    if (value >=0)
+                    {
+                        age = value;
+                    }
+                    else
+                    {
+                        age = 0;
+                    }
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            Person newPerson = new Person("John", "male", 34,"54321987600");
+            Console.WriteLine($"A new person {newPerson.Name}");
+            newPerson.Name = "Johanna";
+            Console.WriteLine($"A new person {newPerson.Name}");
+            newPerson.Gender = "Fairy";
+            Console.WriteLine($"Gender: {newPerson.Gender}");
+            newPerson.Idcode = "54321987603";
+            Console.WriteLine($"IDcode: {newPerson.Idcode}");
+            Console.WriteLine($" {newPerson.Name} age: {newPerson.Age}");
+            
+        }
+    }
+}
---
+++ MovieGetterSetters/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieGetterSetters
+{
+    class Program
+    {
+
+        class Movie
+        {
+            string title;
+            string director;
+            string ratings;
+            int userRating;
+            
+            //kellel film mõeldud on: G, PG, PG-13, R, undefined
+            //userRating not higher than 10 and no lower than 0
+            //saab muuta userratingut hiljem pärast filmi vaatamist 
+
+            public Movie(string _title, string _director, string _ratings, int _userRating)
+            {
+                title = _title;
+                director = _director;
+                Ratings = _ratings;
+                UserRating = _userRating;
+
+
+            }
+
+            public string Title
+            {
+                get { return title; }
+            }
+
+            public string Director
+            {
+                get { return director; }
+            }
+
+            public string Ratings
+            {
+                get { return ratings; }
+                set
+                {
+                    if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                    {
+                        ratings = value;
+                    }
+                    else
+                    {
+                        ratings = "undefined";
+                    }
+                }
+            }
+
+            public int UserRating
+            {
+                get { return userRating; }
+                set
+                {
+                    if (value >=0 && value <=10)
+                    {
+                        userRating = value;
+                    }
+                    else
+                    {
+                        userRating = 0;
+                    }
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+
+            string filePath = @"/Users/katlinpaju/Demo/movies";
+            List<string> listFromFile = File.ReadAllLines(filePath).ToList();
+            List<Movie> listOfMovies=new List<Movie>();
+
+            foreach (string line in listFromFile)
+            {
+                string[] tempArray = line.Split('/');
+                string tempTitel = tempArray[0];
+                string tempDir = tempArray[1];
+                string tempRating = tempArray[2];
+                int tempUserRating = int.Parse(tempArray[3]);
+                
+                Movie tempMovieObject=new Movie(tempTitel, tempDir, tempRating, tempUserRating);
+                listOfMovies.Add(tempMovieObject);
+            }
+
+            int i = 1;
+            foreach (Movie movieObject in listOfMovies)
+            {
+                Console.WriteLine($"Item {i}: {movieObject.Title} directed by {movieObject.Director} ");
+                i++;
+            }
+
+            bool movieFound = false;
+            
+            Console.WriteLine("Enter the key word: ");
+            string userSearch = Console.ReadLine().ToLower();
+            
+            List<Movie> searchResult= new List<Movie>();
+            
+
+            //int searchResult = 0;
+            foreach (Movie movieObject in listOfMovies)
+            {
+                if (movieObject.Title.ToLower().Contains(userSearch))
+                {
+                    searchResult.Add(movieObject);
+                    //Console.WriteLine($"{movieObject.Title} directed by {movieObject.Director}");
+                    //searchResult++;
+                }
+            }
+            
+            Console.WriteLine($"{searchResult.Count} movies found");
+
+            foreach (Movie movieObject in searchResult)
+            {
+                Console.WriteLine($"{movieObject.Title} directed by {movieObject.Director}");
+            }
+            
+            
+            
+
+
+
+
+            /*Movie avangersEndgame=new Movie("Avangers:Endgame", "Anthony Russo", "PG-13", 8 );
+            Console.WriteLine($"A movie: {avangersEndgame.Title}");
+            Console.WriteLine($"Movie directors name: {avangersEndgame.Director}");
+            Console.WriteLine($"Movie rating: {avangersEndgame.Ratings}");
+            Console.WriteLine($"Movie user rating: {avangersEndgame.UserRating}");*/
+
+
+        }
+    }
+}
